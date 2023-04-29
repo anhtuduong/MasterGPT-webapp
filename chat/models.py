71481e5a6c0b7cb5c3ef10fbf848fabb53c30bce
anhtuduong <anhtu.duong@studenti.unitn.it>
2023-04-29 18:00:13 +0200

    init

--- chat/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from MasterGPT.core.Chat import Chat
-
-chat = Chat()
-chat.start()
-
-class ChatDB(models.Model):
-    message = models.TextField()
-    response = models.TextField()
--- chat/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from chat import views
-
-urlpatterns = [
-    path('', views.chat, name='chat'),
-]
